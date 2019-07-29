d91935f5027df96165429f88422b05963571581c
Fred Souza <fredericoamsouza@gmail.com>
2019-07-29 18:19:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const { fork } = require('child_process');
 
-class SPromise {
+module.exports = class SPromise {
 
     constructor(fn, context) {
         return new Promise((resolve, reject) => {
             const child = fork('./worker.js');
             child.send({ type: 'start', payload: { operation: fn+"", context } }, e => e && reject(e));
             child.on('message', ({ type, payload }) => {
-                child.unref();
+                child.kill();
                 if (type === 'resolve') resolve(payload);
                 else reject(payload);
             });
         });
     }
 }
-
-module.exports = SPromise;
