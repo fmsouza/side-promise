6f7f7c7101f5a2a6b257b3acc45306ecd84274ea
Fred Souza <fredericoamsouza@gmail.com>
2019-07-30 12:06:07 -0300

    Improving error handling

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const { fork } = require('child_process');
 
-module.exports = class SPromise {
+module.exports = class SidePromise {
 
     constructor(fn, context) {
         return new Promise((resolve, reject) => {
             const child = fork('./worker.js');
-            child.send({ type: 'start', payload: { operation: fn+"", context } }, e => e && reject(e));
+            child.send({ type: 'start', payload: { operation: fn+"", context } }, e => {
+                if (!e) return;
+                child.kill();
+                reject(e);
+            });
             child.on('message', ({ type, payload }) => {
                 child.kill();
                 if (type === 'resolve') resolve(payload);
